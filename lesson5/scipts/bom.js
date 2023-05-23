5ae8fd7bf4bb7d7534a0f64c3c490f77606a4bce
Monique Roberts <nikitbasil@gmail.com>
2023-05-22 23:08:07 -0600

    bom updates

---
+++ lesson5/scipts/bom.js
@@ -0,0 +1,24 @@
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+  const myItem = input.value;
+  input.value = '';
+
+const newItem =  document.createElement("li");
+const newSpan =  document.createElement("span");
+const newButton =  document.createElement("button");
+
+newItem.appendChild(newSpan);
+newSpan.textContent = myItem;
+newItem.appendChild(newButton);
+newButton.textContent = 'Delete';
+list.appendChild(listItem);
+
+newButton.addEventListener("click", ()=> {  
+list.removeChild(listItem);
+});
+
+input.focus;
+});
