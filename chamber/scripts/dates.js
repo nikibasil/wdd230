06298061cc6e7f54f4de238ad4ed9638614e9d25
Monique Roberts <nikitbasil@gmail.com>
2023-05-24 22:30:31 -0600

    chamged logo and updated greeting script

--- chamber/scripts/dates.js
+++ chamber/scripts/dates.js
@@ -24,24 +24,38 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 
 datefield.textContent = fulldate;
 
-const dayNames = [
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday"
-];
 
-const day = dayNames[now.getDay()]
+const dayNames = {weekday: 'long'};
+let day = new Date().toLocaleDateString('en-US', dayNames);
+const banner = document.querySelector('#greet')
 
-greet = document.getElementById('greet')[0]
+// if (day == 'Monday' || day == "Tuesday") {
+// 	banner.textContent = '🤝 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.'
+// };
 
 if (day != 'Monday' && day != 'Tuesday')
 {
-	greet.style.display = "none"
+	banner.style.display = "none"
 }
+
+// const dayNames = [
+// 	"Sunday",
+// 	"Monday",
+// 	"Tuesday",
+// 	"Wednesday",
+// 	"Thursday",
+// 	"Friday",
+// 	"Saturday"
+// ];
+
+// const day = dayNames[now.getDay()]
+
+// greet = document.getElementById('greet')[0]
+
+// if (day != 'Monday' && day != 'Tuesday')
+// {
+// 	greet.style.display = "none"
+// }
 
 
 
