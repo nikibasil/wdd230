ddb082248b8f63d6785b87cf65992f1c748352f3
Monique Roberts <nikitbasil@gmail.com>
2023-06-02 23:30:56 -0600

    week 7 lazy loading

---
+++ lesson7/scripts/app.js
@@ -0,0 +1,46 @@
+// get all images data-src attribute
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+// optional parameters for intersection observer
+const imgOptions = {
+    threshold: 0.5,
+    rootMargin: "0px 0px 50px 0px"
+}
+
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+      image.removeAttribute("data-src");
+    };
+  };
+
+// check if intersector observor is supported
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) =>
+    {
+        items.forEach((item) =>
+        {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    // loop through each image -check status and load if necessary 
+    imagesToLoad.forEach((img) =>
+    {
+        observer.observe(img);
+    });
+}
+else
+{
+    imagesToLoad.forEach((img) =>
+    {
+        loadImages(img);
+    });
+}
+
+
+
+  
