93194f9c4582c54e213649551a61912e6964eb3d
Monique Roberts <nikitbasil@gmail.com>
2023-06-08 22:22:24 -0600

    lazyload updates

--- lesson7/scripts/app.js
+++ lesson7/scripts/app.js
@@ -40,44 +40,41 @@ const imagesToLoad = document.querySelectorAll("img[data-src]");
 
 // optional parameters for intersection observer
 const imgOptions = {
-    threshold: 0.5,
-    rootMargin: "0px 0px 50px 0px"
-}
-
-const loadImages = (image) => {
+    threshold: 1,
+    rootMargin: "0px 0px 100px 0px"
+    }
+    
+    const loadImages = (image) => {
     image.setAttribute("src", image.getAttribute("data-src"));
     image.onload = () => {
-      image.removeAttribute("data-src");
+    image.removeAttribute("data-src");
     };
-  };
+    };
 
 // check if intersector observor is supported
 if ("IntersectionObserver" in window) {
     const observer = new IntersectionObserver((items, observer) =>
     {
-        items.forEach((item) =>
-        {
-            if (item.isIntersecting) {
-                loadImages(item.target);
-                observer.unobserve(item.target);
-            }
-        });
+    items.forEach((item) =>
+    {
+    if (item.isIntersecting) {
+    loadImages(item.target);
+    observer.unobserve(item.target);
+    }
+    });
     }, imgOptions);
-
-    // loop through each image -check status and load if necessary 
+    
+    // loop through each image -check status and load if necessary
     imagesToLoad.forEach((img) =>
     {
-        observer.observe(img);
+    observer.observe(img);
     });
-}
-else
-{
+    }
+    else
+    {
     imagesToLoad.forEach((img) =>
     {
-        loadImages(img);
+    loadImages(img);
     });
-}
-
-
-
- 
+    }
+    
