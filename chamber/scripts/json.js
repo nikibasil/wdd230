ead5f3af08aa1f8fc75366ac2216ce4630448c3a
Monique Roberts <nikitbasil@gmail.com>
2023-06-16 02:16:13 -0600

    update to json file

--- chamber/scripts/json.js
+++ chamber/scripts/json.js
@@ -4,10 +4,14 @@ getBusinessData(url);
 async function getBusinessData(url) {
     const response = await fetch(url);
    
-    // if (response.ok) {
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data.business);
+        displayBusiness(data.business);
+    }
         
-    const data = await response.json();
-    console.table(data.business);  
+    
+      
     // note that we reference the prophet array of the data object given the structure of the json file
     // document.getElementById("prophet").innerHTML = await myPromise;
     displayBusiness(data.business);
@@ -20,7 +24,7 @@ async function getBusinessData(url) {
   const displayBusiness = (business) => {
     const cards = document.querySelector('div.cards'); // select the output container element
   
-    business.forEach((bus) => {
+    business.forEach((business) => {
       // Create elements to add to the div.cards element
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
@@ -28,11 +32,11 @@ async function getBusinessData(url) {
       let logo = document.createElement('img');
   
       // Build the h2 content out to show the prophet's full name - finish the template string
-      h2.textContent = `${bus.name} `;
-      address.innerHTML = `<strong>Address</strong>: ${bus.address}`;
+      h2.textContent = `${business.name} `;
+      address.innerHTML = `<strong>Address</strong>: ${business.address}`;
       // Build the image logo by setting all the relevant attribute
-      logo.setAttribute('src', bus.imageurl);
-      logo.setAttribute('alt', `Logo of ${bus.name}`);
+      logo.setAttribute('src', business.imageurl);
+      logo.setAttribute('alt', `Logo of ${business.name}`);
       logo.setAttribute('loading', 'lazy');
       logo.setAttribute('width', '340');
       logo.setAttribute('height', '440');
