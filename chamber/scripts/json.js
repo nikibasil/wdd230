623833a5270cafc9a24d19d6173b18e8fc3cdde7
Monique Roberts <nikitbasil@gmail.com>
2023-06-16 02:09:58 -0600

    start of business directory

---
+++ chamber/scripts/json.js
@@ -0,0 +1,48 @@
+const url = 'https://nikibasil.github.io/wdd230/chamber/json/data.json';
+getBusinessData(url);
+
+async function getBusinessData(url) {
+    const response = await fetch(url);
+   
+    // if (response.ok) {
+        
+    const data = await response.json();
+    console.table(data.business);  
+    // note that we reference the prophet array of the data object given the structure of the json file
+    // document.getElementById("prophet").innerHTML = await myPromise;
+    displayBusiness(data.business);
+    }
+
+// }
+  
+  
+
+  const displayBusiness = (business) => {
+    const cards = document.querySelector('div.cards'); // select the output container element
+  
+    business.forEach((bus) => {
+      // Create elements to add to the div.cards element
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let address = document.createElement('p');
+      let logo = document.createElement('img');
+  
+      // Build the h2 content out to show the prophet's full name - finish the template string
+      h2.textContent = `${bus.name} `;
+      address.innerHTML = `<strong>Address</strong>: ${bus.address}`;
+      // Build the image logo by setting all the relevant attribute
+      logo.setAttribute('src', bus.imageurl);
+      logo.setAttribute('alt', `Logo of ${bus.name}`);
+      logo.setAttribute('loading', 'lazy');
+      logo.setAttribute('width', '340');
+      logo.setAttribute('height', '440');
+  
+      // Append the section(card) with the created elements
+      card.appendChild(h2);
+      card.appendChild(address);
+      card.appendChild(logo);
+      // card.append(p)
+      cards.appendChild(card);
+    } )// end of forEach loop
+  }
+   // end of function expression
---
+++ chamber/scripts/prophet.js
@@ -0,0 +1,48 @@
+const url = 'json/data.json';
+getProphetData(url);
+
+async function getProphetData(url) {
+    const response = await fetch(url);
+   
+    // if (response.ok) {
+        
+    const data = await response.json();
+    console.table(data.prophets);  
+    // note that we reference the prophet array of the data object given the structure of the json file
+    // document.getElementById("prophet").innerHTML = await myPromise;
+    displayProphets(data.prophets);
+    }
+
+// }
+  
+  
+
+  const displayProphets = (prophets) => {
+    const cards = document.querySelector('div.cards'); // select the output container element
+  
+    prophets.forEach((prophet) => {
+      // Create elements to add to the div.cards element
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let address = document.createElement('p');
+      let portrait = document.createElement('img');
+  
+      // Build the h2 content out to show the prophet's full name - finish the template string
+      h2.textContent = `${prophet.name} `;
+      birthdate.innerHTML = `<strong>Date of Birth</strong>: ${prophet.address}<br><strong>Place of Birth</strong>: ${prophet.phone}`;
+      // Build the image portrait by setting all the relevant attribute
+      portrait.setAttribute('src', prophet.imageurl);
+      portrait.setAttribute('alt', `Portait of ${prophet.name}`);
+      portrait.setAttribute('loading', 'lazy');
+      portrait.setAttribute('width', '340');
+      portrait.setAttribute('height', '440');
+  
+      // Append the section(card) with the created elements
+      card.appendChild(h2);
+      card.appendChild(birthdate);
+      card.appendChild(portrait);
+      // card.append(p)
+      cards.appendChild(card);
+    } )// end of forEach loop
+  }
+   // end of function expression
