3779c67375e1337b6b3001c0331c7abaa469cf06
Monique Roberts <nikitbasil@gmail.com>
2023-06-18 00:23:16 -0600

    directory images and js filter buttons

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -22,19 +22,23 @@ async function getBusinessData(url) {
   
 
   const displayBusiness = (business) => {
-    const cards = document.querySelector('div.cards'); // select the output container element
+    const cards = document.querySelector('article.cards'); // select the output container element
   
     business.forEach((business) => {
       // Create elements to add to the div.cards element
       let card = document.createElement('section');
-      let h2 = document.createElement('h2');
+      let name = document.createElement('h2');
       let address = document.createElement('p');
       let logo = document.createElement('img');
+      let phone = document.createElement('p');
+      let website = document.createElement('p');
   
       // Build the h2 content out to show the prophet's full name - finish the template string
-      h2.textContent = `${business.name} `;
+      name.textContent = `${business.name} `;
       address.innerHTML = `<strong>Address</strong>: ${business.address}`;
       // Build the image logo by setting all the relevant attribute
+      phone.textContent = `${business.phone}`;
+      website.textContent = `${business.website}`;
       logo.setAttribute('src', business.imageurl);
       logo.setAttribute('alt', `Logo of ${business.name}`);
       logo.setAttribute('loading', 'lazy');
@@ -42,23 +46,26 @@ async function getBusinessData(url) {
       logo.setAttribute('height', '440');
   
       // Append the section(card) with the created elements
-      card.appendChild(h2);
-      card.appendChild(address);
+      card.appendChild(name);
       card.appendChild(logo);
+      card.appendChild(address);
+      card.appendChild(phone);
+      card.appendChild(website);
+    
       // card.append(p)
       cards.appendChild(card);
     } )// end of forEach loop
   }
    // end of function expression
 
-   const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
+   const gridbutton = document.querySelector("#gridBtn");
+const listbutton = document.querySelector("#listBtn");
 const display = document.querySelector("article");
 
 
 gridbutton.addEventListener("click", () => {
 	// example using arrow function
-	display.classList.add("grid");
+	display.classList.add("cards");
 	display.classList.remove("list");
 });
 
@@ -66,5 +73,5 @@ listbutton.addEventListener("click", showList); // example using defined functio
 
 function showList() {
 	display.classList.add("list");
-	display.classList.remove("grid");
+	display.classList.remove("cards");
 }
