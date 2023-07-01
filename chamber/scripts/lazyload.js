a1466915c096e1afff8d4da490d5f3adf5b9bb71
Monique Roberts <nikitbasil@gmail.com>
2023-06-30 21:19:47 -0600

    fixed hidden date error

--- chamber/scripts/lazyload.js
+++ chamber/scripts/lazyload.js
@@ -1,40 +1,3 @@
-//
-// const toLoad = document.querySelectorAll("[data-src]");
-
-// function preloadImage(img) {
-//     const src = img.getAttribute('data-src');
-//     if(!src) {
-//         return;
-//     } 
-
-//     img.src = src;
-//     console.log("finished with preloadImg function");
-// }
-
-
-// imgOptions = {
-//     threshold: 0,
-//     rootMargin: "200px 0px 100px 0px"
-// };
-
-// const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-//     entries.forEach(entry => {
-//         if (!entry.isIntersecting) {
-//             return;
-//         } else {
-//             preloadImage(entry.target);
-//             imgObserver.unobserve(entry.target);
-//             console.log("finished with imgObserver function");
-//         }
-//     })
-// }, imgOptions);
-
-
-// toLoad.forEach(image => {
-//     imgObserver.observe(image);
-//     console.log("finished with toLoad function");
-// });
-
 // get all images data-src or srcset attribute
 const imagesToLoad = document.querySelectorAll("img[data-src]");
 
@@ -78,6 +41,49 @@ else
     });
 }
 
+
+
+
+
+
+
+
+//
+// const toLoad = document.querySelectorAll("[data-src]");
+
+// function preloadImage(img) {
+//     const src = img.getAttribute('data-src');
+//     if(!src) {
+//         return;
+//     } 
+
+//     img.src = src;
+//     console.log("finished with preloadImg function");
+// }
+
+
+// imgOptions = {
+//     threshold: 0,
+//     rootMargin: "200px 0px 100px 0px"
+// };
+
+// const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+//     entries.forEach(entry => {
+//         if (!entry.isIntersecting) {
+//             return;
+//         } else {
+//             preloadImage(entry.target);
+//             imgObserver.unobserve(entry.target);
+//             console.log("finished with imgObserver function");
+//         }
+//     })
+// }, imgOptions);
+
+
+// toLoad.forEach(image => {
+//     imgObserver.observe(image);
+//     console.log("finished with toLoad function");
+// });
 
 
  
